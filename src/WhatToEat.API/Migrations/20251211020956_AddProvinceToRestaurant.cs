c306cafeab03d22810e8fbacc07b2863edf11895
Ricardo Georgel <ricardo@justbuildit.com>
2025-12-10 21:11:44 -0500

    update migration

--- src/WhatToEat.API/Migrations/20251211020956_AddProvinceToRestaurant.cs
+++ src/WhatToEat.API/Migrations/20251211020956_AddProvinceToRestaurant.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -35,5 +35,6 @@ namespace WhatToEat.API.Migrations
                 name: "Province",
                 table: "Restaurants");
         }
+
     }
 }
